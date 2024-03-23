fea5bcce732dc77433c7ae8074ecc046bdde05c3
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-23 06:26:59 -0300

    Arquvios deletados

--- vitalHub/src/components/BrandLogo/style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const BrandLogoBlue = styled.Image.attrs({
-  source: require('../../assets/images/VitalHub_Logo_Azul.png')
-})`
-  width:214px;
-  height:120px;
-`
