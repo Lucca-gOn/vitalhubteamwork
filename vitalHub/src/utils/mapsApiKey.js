4f950cc82305d50b7059e24da07e9438d32db98e
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-24 12:35:03 -0300

    Exportando a MapsKey

---
+++ vitalHub/src/utils/mapsApiKey.js
@@ -0,0 +1,8 @@
+// Esta chave de acesso deve ser criada atraves do google cloud para mais informações ler documentaçao
+
+/**
+ * apikey: Your Google Maps Directions API Key (request one here; if you're using an existing Google Maps API Key make sure you've enabled the Google Maps Directions API for that key using the Google API Console).
+ * https://console.cloud.google.com/projectselector2/apis/dashboard?supportedpurview=project
+ */
+
+export const mapskey = "AIzaSyAqIF5MN4yo8ZHwg9WsKP14I-fAK6hpKWY";
