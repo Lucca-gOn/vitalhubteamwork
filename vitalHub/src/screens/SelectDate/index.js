5e6bfb1f61b3bc2a6c9202c65aa7ac53ba2c7b24
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-24 12:34:39 -0300

    Realizado alterações o select para funcionar corretamente

--- vitalHub/src/screens/SelectDate/index.js
+++ vitalHub/src/screens/SelectDate/index.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { ContainerMargin, ContainerMarginStatusBar } from "../../components/Conatainer";
 import { TextLabelBlack, Title } from "../../components/Texts/style";
 import { useState } from "react";
@@ -10,6 +10,8 @@ import { CalendarMonth } from "../../components/Calendars";
 import RNPickerSelect from 'react-native-picker-select';
 import { FontAwesome6 } from '@expo/vector-icons';
 import { Dimensions } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { SummaryMedicalAgenda } from "../../components/Modals";
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -20,22 +22,22 @@ export default function SelectDate({
   const [select, setSelect] = useState(null)
 
   const timeCLinic = [
-    {label: '08:00', value:'08:00'},
-    {label: '09:00', value:'09:00'},
-    {label: '10:00', value:'10:00'},
-    {label: '11:00', value:'11:00'},
-    {label: '12:00', value:'12:00'},
-    {label: '13:00', value:'13:00'},
-    {label: '14:00', value:'14:00'},
-    {label: '15:00', value:'15:00'},
-    {label: '16:00', value:'16:00'},
-    {label: '17:00', value:'17:00'},
-    {label: '18:00', value:'18:00'},
-    {label: '19:00', value:'19:00'},
-    {label: '20:00', value:'20:00'},
+    { label: '08:00', value: '08:00' },
+    { label: '09:00', value: '09:00' },
+    { label: '10:00', value: '10:00' },
+    { label: '11:00', value: '11:00' },
+    { label: '12:00', value: '12:00' },
+    { label: '13:00', value: '13:00' },
+    { label: '14:00', value: '14:00' },
+    { label: '15:00', value: '15:00' },
+    { label: '16:00', value: '16:00' },
+    { label: '17:00', value: '17:00' },
+    { label: '18:00', value: '18:00' },
+    { label: '19:00', value: '19:00' },
+    { label: '20:00', value: '20:00' },
   ]
 
-    
+
   return (
     <ContainerMarginStatusBar
       // $bgColor="pink"
@@ -51,43 +53,48 @@ export default function SelectDate({
         <TextLabelBlack>Selecione um horário disponível:</TextLabelBlack>
 
 
-        {/* <View style={{width:'100%', borderWidth:2,borderColor:'#34898F',borderStyle:"solid", borderRadius:5}}> */}
+        <View style={{ width: '100%', borderWidth: 2, borderColor: '#34898F', borderStyle: "solid", borderRadius: 5 }}>
 
-       
+
 
 
           <RNPickerSelect
             useNativeAndroidPickerStyle={false}
+            fixAndroidTouchableBug={true}
             onValueChange={(value) => console.log(value)}
             items={timeCLinic}
             placeholder={{ label: 'Seleciona Horário', value: null }}
-
-            fixAndroidTouchableBug={false}
+            Icon={() => {
+              return (
+                <TouchableOpacity>
+                  <AntDesign name="caretdown" size={14} color="#34898F" style={{ backgroundColor: 'pink' }} />
+                </TouchableOpacity>
+              )
+            }}
             style={{
-              headlessAndroidPicker:{
-                padding:0
+              iconContainer: {
+                height: '100%',
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 16,
+                zIndex: 0
               },
-              inputWeb:{
-                padding:0
+              headlessAndroidContainer: {
+                justifyContent: "center",
               },
-              viewContainer:{
-                padding:0,
-                margin:0
-              },
-              headlessAndroidContainer:{
-                padding:0,
-                margin:0,
-                alignItems:"center",
-                justifyContent:"center",
-                height:30,
-                backgroundColor:'black'
-              },              
-              inputAndroidContainer:{
-                // padding:16
+              inputIOS: {
+                color: '#34898F',
+                padding: 16,
               },
               inputAndroid: {
                 color: '#34898F',
-                backgroundColor:'pink',           
+                padding: 16,
+                width: '100%',
+                zIndex: 10,
+              },
+              inputWeb: {
+                color: '#34898F',
+                padding: 16,
               },
               placeholder: {
                 color: '#34898F',
@@ -95,14 +102,13 @@ export default function SelectDate({
                 fontSize: 14,
                 height: 'auto',
               }
-              
             }}
           />
-        {/* </View> */}
+        </View>
       </ContainerMargin>
 
 
-      <ContainerMargin $mt={42} $mb={35} $gap={30} $width="80%">
+      <ContainerMargin $mt={42} $mb={30} $gap={30} $width="80%">
         <ButtonDefault textButton="Continuar" />
 
         <LinkUnderlineDefault
@@ -112,6 +118,8 @@ export default function SelectDate({
         </LinkUnderlineDefault>
       </ContainerMargin>
 
+
+      <SummaryMedicalAgenda />
     </ContainerMarginStatusBar>
   )
 }
