c31bf5cff34429d9768114708e8616fdf178aae0
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-25 20:12:29 -0300

    Adicionado modal da Cemara e alterado os parametros de rota.

--- vitalHub/src/screens/MedicalRecord/index.js
+++ vitalHub/src/screens/MedicalRecord/index.js
@@ -8,19 +8,23 @@ import { ButtonDefault, ButtonGreen } from "../../components/Buttons"
 import { LinkUnderlineDefault } from "../../components/Links"
 import { Stethoscope } from "../../components/Stethoscope"
 import { Image } from "expo-image"
+import { ModalCamera } from "../../components/Modals"
+import { useState } from "react"
 
 
-export default  function MedicalRecord ({
+export default function MedicalRecord({
   navigation,
   route
 }) {
 
-  const {name, age, email, photo} = route.params
+  const { name, age, email, photo, fotoCam } = route.params
+  const [showModalCamera, setShowModalCamera] = useState(false)
 
+  console.log(fotoCam)
   return (
     <Container>
 
-      <StatusBar translucent={true} barStyle="light-content" backgroundColor={'transparent'}/>
+      <StatusBar translucent={true} barStyle="light-content" backgroundColor={'transparent'} />
 
       <ImageUser source={photo !== undefined ? { uri: photo } : require('../../assets/images/NotImage.svg')} $width="100%" $height="280px" />
 
@@ -28,59 +32,67 @@ export default  function MedicalRecord ({
         showsVerticalScrollIndicator={false}
       >
 
-      
-      <ContainerMargin $mt={20} $width="100%">
-        <Title>
-          {name}
-        </Title>
-      </ContainerMargin>
-      <ContainerMargin $width="80%" $mt={18} $mb={24} $fd="row" $justContent="space-around">
-        <TextQuickSandRegular>
-          {age}
-        </TextQuickSandRegular>
-        <TextQuickSandRegular>
-          {email}
-        </TextQuickSandRegular>
-      </ContainerMargin>
 
-      <ContainerMargin $alingItens="flex-start" $gap={10}>
-        <TextLabel>Descrição da consulta</TextLabel>
-        <InputGreenMultiLine placeholder="Descrição"/>
-      </ContainerMargin>
-
-      <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
-        <TextLabel>Diagnóstico do paciente</TextLabel>
-        <InputGreen placeholder="Diagnóstico"/>
-      </ContainerMargin>
-
-      <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
-        <TextLabel>Prescrição médica</TextLabel>
-        <InputGreenMultiLine placeholder="Prescrição medica"/>
-      </ContainerMargin>
-
-      <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
-        <TextLabel>Exames médicos</TextLabel>
-        <ContainerMargin $width="100%" $height="111px" $bgColor="#F5F3F3" $borderRadius={5} $fd="row" $gap={9}>
-          <Image source={require('../../assets/images/ImageExclamation.svg')} style={{width: 16, height:18}} />
-          <TextInformation>Nenhuma foto informada</TextInformation>
+        <ContainerMargin $mt={20} $width="100%">
+          <Title>
+            {name}
+          </Title>
         </ContainerMargin>
-      </ContainerMargin>
+        <ContainerMargin $width="80%" $mt={18} $mb={24} $fd="row" $justContent="space-around">
+          <TextQuickSandRegular>
+            {age}
+          </TextQuickSandRegular>
+          <TextQuickSandRegular>
+            {email}
+          </TextQuickSandRegular>
+        </ContainerMargin>
 
-      <ContainerMargin $fd="row" $justContent="space-between" $mt={10}>
-        <ButtonGreen/>
-        <TextCancelAppointment style={{width:'25%',paddingTop:10,paddingBottom:10 }}>Cancelar</TextCancelAppointment>
-      </ContainerMargin>   
+        <ContainerMargin $alingItens="flex-start" $gap={10}>
+          <TextLabel>Descrição da consulta</TextLabel>
+          <InputGreenMultiLine placeholder="Descrição" />
+        </ContainerMargin>
 
-      <ContainerMargin $mt={30} $gap={30} $mb={30}>
+        <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
+          <TextLabel>Diagnóstico do paciente</TextLabel>
+          <InputGreen placeholder="Diagnóstico" />
+        </ContainerMargin>
+
+        <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
+          <TextLabel>Prescrição médica</TextLabel>
+          <InputGreenMultiLine placeholder="Prescrição medica" />
+        </ContainerMargin>
+
+        <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
+          <TextLabel>Exames médicos</TextLabel>
+          <ContainerMargin $width="100%" $height="111px" $bgColor="#F5F3F3" $borderRadius={5} $fd="row" $gap={9}>
+            {fotoCam !== undefined ? 
+            
+              <Image source={fotoCam !== undefined ? {uri: fotoCam.photoCam} : require('../../assets/images/NotImage.svg')} style={{ width: "100%", height: '100%', resizeMode:"cover"}} />
+             :              
+             <>
+                  <Image source={require('../../assets/images/ImageExclamation.svg')} style={{ width: 16, height: 18 }} />
+                  <TextInformation>Nenhuma foto informada</TextInformation>
+                </>
+              
+            }
+          </ContainerMargin>
+        </ContainerMargin>
+
+        <ContainerMargin $fd="row" $justContent="space-between" $mt={10}>
+          <ButtonGreen onPress={() => { setShowModalCamera(true) }} />
+          <TextCancelAppointment style={{ width: '25%', paddingTop: 10, paddingBottom: 10 }}>Cancelar</TextCancelAppointment>
+        </ContainerMargin>
+
+        <ContainerMargin $mt={30} $gap={30} $mb={30}>
           <ButtonDefault textButton="Salvar" />
 
           <ButtonDefault textButton="Editar" />
 
-          <LinkUnderlineDefault onPress={()=> {navigation.replace('Home')}}>Cancelar</LinkUnderlineDefault>
+          <LinkUnderlineDefault onPress={() => { navigation.replace('Home') }}>Cancelar</LinkUnderlineDefault>
         </ContainerMargin>
       </ContainerScrollView>
 
-     
+      <ModalCamera showModalCamera={showModalCamera} setShowModalCamera={setShowModalCamera} navigation={navigation} />
     </Container>
   )
 }
