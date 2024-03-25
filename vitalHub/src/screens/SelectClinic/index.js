06440f448e8f943e7a2119c58f9ce09c48be591f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-25 20:13:26 -0300

    Corrigido o navigation do botão cancelar

--- vitalHub/src/screens/SelectClinic/index.js
+++ vitalHub/src/screens/SelectClinic/index.js
@@ -13,53 +13,56 @@ export default function SelectClinic({
   const [select, setSelect] = useState(null)
 
   const clinicas = [
-    {id:1,company: 'Clínica Natureh', grade: 4.5, city: 'São Paulo', uf:'SP', open: 'Seg-Sex' },
-    {id:2,company: 'Diamond Pró-Mulher', grade: 4.8, city: 'São Paulo', uf:'SP', open: 'Seg-Sex' },
-    {id:3,company: 'Clinica Villa Lobos', grade: 4.2, city: 'Taboão', uf:'SP', open: 'Seg-Sab' },
-    {id:4,company: 'SP Oncologia Clínica', grade: 4.2, city: 'Taboão', uf:'SP', open: 'Seg-Sab' },
-    {id:5,company: 'Policlínica Centro', grade: 5, city: 'São Bernardo do Campo', uf:'SP', open: 'Seg-Sab' },
-    {id:6,company: 'Clínica Natureh', grade: 4.5, city: 'São Paulo', uf:'SP', open: 'Seg-Sex' },
-    {id:7,company: 'Diamond Pró-Mulher', grade: 4.8, city: 'São Paulo', uf:'SP', open: 'Seg-Sex' },
-    {id:8,company: 'Clinica Villa Lobos', grade: 4.2, city: 'Taboão', uf:'SP', open: 'Seg-Sab' },
-    {id:9,company: 'SP Oncologia Clínica', grade: 4.2, city: 'Taboão', uf:'SP', open: 'Seg-Sab' },
-    {id:10,company: 'Policlínica Centro', grade: 5, city: 'São Bernardo do Campo', uf:'SP', open: 'Seg-Sab' },
+    { id: 1, company: 'Clínica Natureh', grade: 4.5, city: 'São Paulo', uf: 'SP', open: 'Seg-Sex' },
+    { id: 2, company: 'Diamond Pró-Mulher', grade: 4.8, city: 'São Paulo', uf: 'SP', open: 'Seg-Sex' },
+    { id: 3, company: 'Clinica Villa Lobos', grade: 4.2, city: 'Taboão', uf: 'SP', open: 'Seg-Sab' },
+    { id: 4, company: 'SP Oncologia Clínica', grade: 4.2, city: 'Taboão', uf: 'SP', open: 'Seg-Sab' },
+    { id: 5, company: 'Policlínica Centro', grade: 5, city: 'São Bernardo do Campo', uf: 'SP', open: 'Seg-Sab' },
+    { id: 6, company: 'Clínica Natureh', grade: 4.5, city: 'São Paulo', uf: 'SP', open: 'Seg-Sex' },
+    { id: 7, company: 'Diamond Pró-Mulher', grade: 4.8, city: 'São Paulo', uf: 'SP', open: 'Seg-Sex' },
+    { id: 8, company: 'Clinica Villa Lobos', grade: 4.2, city: 'Taboão', uf: 'SP', open: 'Seg-Sab' },
+    { id: 9, company: 'SP Oncologia Clínica', grade: 4.2, city: 'Taboão', uf: 'SP', open: 'Seg-Sab' },
+    { id: 10, company: 'Policlínica Centro', grade: 5, city: 'São Bernardo do Campo', uf: 'SP', open: 'Seg-Sab' },
   ]
 
-  return(
+  return (
     <ContainerMarginStatusBar
       $bgColor="#FBFBFB"
     >
-    <ContainerMargin $mt={30} $mb={30}>
-      <Title>Selecionar clínica</Title>
-    </ContainerMargin>
+      <ContainerMargin $mt={30} $mb={30}>
+        <Title>Selecionar clínica</Title>
+      </ContainerMargin>
 
-    <FlatList
-      data={clinicas}
-      keyExtractor={(item) => item.id}
-      renderItem={({item})=>(
-        <ClinicCardData 
-          data={item}
-          onPress={()=> setSelect(item.id)}
-          select={select === item.id}
-        />
-      )}
-      style={{
-        width:'90%',
-      }}
-      showsVerticalScrollIndicator={false}
-    >
+      <FlatList
+        data={clinicas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ClinicCardData
+            data={item}
+            onPress={() => setSelect(item.id)}
+            select={select === item.id}
+          />
+        )}
+        style={{
+          width: '90%',
+        }}
+        showsVerticalScrollIndicator={false}
+      >
 
-    </FlatList>
+      </FlatList>
 
-    <ContainerMargin $mt={30} $mb={35} $gap={30} $width="80%">
-              <ButtonDefault textButton="Inserir prontuário" />
+      <ContainerMargin $mt={30} $mb={35} $gap={30} $width="80%">
+        <ButtonDefault textButton="Continuar" onPress={() => navigation.navigate('SelectMedic')} />
 
-              <LinkUnderlineDefault
-                onPress={()=> setShowModalScheduleAppointment(false)}
-              >
-                Cancelar
-              </LinkUnderlineDefault>
-            </ContainerMargin>
+        <LinkUnderlineDefault onPress={() => {
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'Main' }]
+          })
+        }}>
+          Cancelar
+        </LinkUnderlineDefault>
+      </ContainerMargin>
 
     </ContainerMarginStatusBar>
   )
