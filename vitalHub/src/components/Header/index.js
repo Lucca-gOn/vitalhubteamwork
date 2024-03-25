18e0c31bed5e6975ebba83193725c222dd5c6652
Allan Rodriues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-25 10:52:35 -0300

    Adicionado Style ao Touchable do Header

--- vitalHub/src/components/Header/index.js
+++ vitalHub/src/components/Header/index.js
@@ -32,7 +32,7 @@ export const Header = (
       <LinearGradienteHeader>
         <ContainerMargin $fd="row" $justContent="space-between" $width="100%" $pd="62px 5% 22px 5%">
           <ContainerMargin $fd="row" $gap={10} $width="auto">
-            <TouchableOpacity onPress={()=>navigation.navigate('Profile')}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Profile')} style={{flexDirection:"row", gap: 10, alignItems: "center"}}>
             <ImageUser source={{ uri: 'https://github.com/AllanR1991.png' }} />
             <ContainerMargin $alingItens="flex-start" $justContent="flex-start" $width="auto" $gap={3}>
               <TitleHeader>Bem vindo!</TitleHeader>
