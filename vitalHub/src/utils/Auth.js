44f75813f6b2594aa24e41eb7d8c2d5d12f43ad2
Allan Rodriues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-25 10:04:47 -0300

    Realizado função para decodificaçào e codificação do token

---
+++ vitalHub/src/utils/Auth.js
@@ -0,0 +1,29 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode"; //serviço para decodificar o token
+import { decode, encode} from 'base-64';
+
+if( !global.atob ){
+  global.atob = decode
+}
+
+if( !global.btoa){
+  global.btoa = encode
+}
+
+export const userDecodeToken = async () => {
+  //Capturando o token
+  const token = await AsyncStorage.getItem('token');
+  if( token === null){
+    return null;
+  }
+
+  //descriptografando o token
+  const decoded = jwtDecode(token)
+
+  return {
+    role: decoded.role,
+    name: decoded.name,
+    email: decoded.email,
+  }
+}
+
