407a0555fe1cdff22510c636a6388d45d7117979
Allan Rodriues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-27 08:28:10 -0300

    Removido api

--- Scripts/script2.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [ID]
-      ,[TipoUsuario]
-  FROM [VitalHub_G09M].[dbo].[TiposUsuario]
-
-  INSERT INTO [VitalHub_G09M].[dbo].[TiposUsuario] ([ID], [TipoUsuario])
-VALUES (NEWID(), 'Paciente'),
-       (NEWID(), 'Médico');
