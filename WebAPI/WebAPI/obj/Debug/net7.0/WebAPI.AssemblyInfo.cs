c557732b55f4f775862e5db916730676e38aa171
Lucas Oliveira <lucas.gon.oliv@gmail.com>
2024-03-27 09:26:33 -0300

    .

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ec3dbe0206d2f2a79a4b6793926b53bf148fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0190715b2a8b05a18684f2ba945de0a98ee746")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
