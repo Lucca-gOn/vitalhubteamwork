4156aeb61fd47442075b688b53ef201b401a9493
Allan Rodriues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-05 10:51:00 -0300

    .

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ac015b23736759675497b30df7e735abcebb81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638b99262193d4a711149c963559f955ed81e306")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
