c59b46291acff076e3b444b751c99724ec428a22
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-06 12:20:13 -0300

    d

--- vitalHub/src/screens/Home/index.js
+++ vitalHub/src/screens/Home/index.js
@@ -39,11 +39,14 @@ export default function Home(
 
   async function ListarConsultas(){
 
-    const url = ( profile.role == 'Medico' ? 'Medicos': 'Pacientes')
+    const url = ( profile.role == 'Medico' ?  'Medicos' : 'Pacientes' )
     // console.log('ListarConsulta Valor de Url: ', url);
     // console.log('ListarConsulta Valor de DateConsult: ', dateConsult)
     // console.log('Listar Consulta valor de id: ', profile.id)
-
+    
+    console.log(url)
+    console.log(dateConsult)
+    console.log(profile.id)
     await api.get(`/${url}/BuscarPorData?data=${dateConsult}&id=${profile.id}`)
     .then(response => {
       setConsultas(response.data);
@@ -67,6 +70,8 @@ export default function Home(
       ListarConsultas();
     }
   },[dateConsult])
+
+  // console.log(consultas)
 
   return (
   
