cbdfdca87f271e0aa87baa781fdc76057eaab3db
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-06 12:22:23 -0300

    Recebido os parametros vindos pela rota e adicionado em contanstes, realizado tambem o logica para ocultar a parte do exame caso ele seja medico e deixar o campos desabilidadtos

--- vitalHub/src/screens/MedicalRecord/index.js
+++ vitalHub/src/screens/MedicalRecord/index.js
@@ -9,90 +9,147 @@ import { LinkUnderlineDefault } from "../../components/Links"
 import { Stethoscope } from "../../components/Stethoscope"
 import { Image } from "expo-image"
 import { ModalCamera } from "../../components/Modals"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function MedicalRecord({
   navigation,
   route
-}) {
-
-  const { name, age, email, photo, fotoCam } = route.params || {}
+}) {  
+  const nomePaciente = route.params.dadosConsulta.paciente.idNavigation.nome;
+  const email = route.params.dadosConsulta.paciente.idNavigation.email;
+  const descricaoConsulta = route.params.dadosConsulta.descricao ;
+  const diagnosticoPaciente = route.params.dadosConsulta.diagnostico;
+  const prescricaoMedica = route.params.dadosConsulta.receita;
+  const idade = route.params.idade
+  const foto = route.params.dadosConsulta.paciente.idNavigation.foto;
+  const role = route.params.role;
+  const fotoCam = route.params.fotoCam
+  
   const [showModalCamera, setShowModalCamera] = useState(false)
+  const [disabledInput, setDisableInput] = useState(false)
 
-  console.log(fotoCam)
+  function verificaProntuario(){
+  
+    diagnosticoPaciente!== null || diagnosticoPaciente !== null || prescricaoMedica !== null ?
+    setDisableInput(true) :
+      setDisableInput(false)
+  }
+
+  useEffect(()=>{
+    verificaProntuario()
+  },[])
   return (
     <Container>
 
       <StatusBar translucent={true} barStyle="light-content" backgroundColor={'transparent'} />
 
-      <ImageUser source={photo !== undefined ? { uri: photo } : require('../../assets/images/NotImage.svg')} $width="100%" $height="280px" />
+      <ImageUser source={foto !== undefined && foto !== 'string' ? { uri: foto } : require('../../assets/images/NotImage.svg')} $width="100%" $height="280px" />
 
       <ContainerScrollView
         showsVerticalScrollIndicator={false}
       >
 
-
         <ContainerMargin $mt={20} $width="100%">
+      
           <Title>
-            {name}
+            {nomePaciente}
           </Title>
+      
         </ContainerMargin>
+        
         <ContainerMargin $width="80%" $mt={18} $mb={24} $fd="row" $justContent="space-around">
+      
           <TextQuickSandRegular>
-            {age}
+            {
+              idade < 2 ?
+                idade + ' ano' :
+                idade + ' anos'
+            }
           </TextQuickSandRegular>
+      
           <TextQuickSandRegular>
-            {email}
+            {email}         
           </TextQuickSandRegular>
+        
         </ContainerMargin>
 
         <ContainerMargin $alingItens="flex-start" $gap={10}>
+        
           <TextLabel>Descrição da consulta</TextLabel>
-          <InputGreenMultiLine placeholder="Descrição" />
+        
+          <InputGreenMultiLine placeholder="Descrição" editable={!disabledInput} disabledInput={disabledInput} value={descricaoConsulta} />
+        
         </ContainerMargin>
 
         <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
+        
           <TextLabel>Diagnóstico do paciente</TextLabel>
-          <InputGreen placeholder="Diagnóstico" />
+        
+          <InputGreen placeholder="Diagnóstico" editable={!disabledInput} disabledInput={disabledInput} value={diagnosticoPaciente} />
+        
         </ContainerMargin>
 
         <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
+        
           <TextLabel>Prescrição médica</TextLabel>
-          <InputGreenMultiLine placeholder="Prescrição medica" />
+        
+          <InputGreenMultiLine editable={!disabledInput} placeholder="Prescrição medica" disabledInput={disabledInput} value={prescricaoMedica} />
+        
         </ContainerMargin>
 
-        <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
-          <TextLabel>Exames médicos</TextLabel>
-          <ContainerMargin $width="100%" $height="111px" $bgColor="#F5F3F3" $borderRadius={5} $fd="row" $gap={9}>
-            {fotoCam !== undefined ? 
-            
-              <Image source={fotoCam !== undefined ? {uri: fotoCam.photoCam} : require('../../assets/images/NotImage.svg')} style={{ width: "100%", height: '100%', resizeMode:"cover"}} />
-             :              
-             <>
-                  <Image source={require('../../assets/images/ImageExclamation.svg')} style={{ width: 16, height: 18 }} />
-                  <TextInformation>Nenhuma foto informada</TextInformation>
-                </>
-              
-            }
-          </ContainerMargin>
-        </ContainerMargin>
+        {
+          role !== 'Medico' ? (
+            <>
 
-        <ContainerMargin $fd="row" $justContent="space-between" $mt={10}>
-          <ButtonGreen onPress={() => { setShowModalCamera(true) }} />
-          <TextCancelAppointment style={{ width: '25%', paddingTop: 10, paddingBottom: 10 }}>Cancelar</TextCancelAppointment>
-        </ContainerMargin>
+
+              <ContainerMargin $alingItens="flex-start" $gap={10} $mt={20}>
+        
+                <TextLabel>Exames médicos</TextLabel>
+        
+                <ContainerMargin $width="100%" $height="111px" $bgColor="#F5F3F3" $borderRadius={5} $fd="row" $gap={9}>
+                  {fotoCam !== undefined ?
+
+                    <Image source={fotoCam !== undefined ? { uri: fotoCam.photoCam } : require('../../assets/images/NotImage.svg')} style={{ width: "100%", height: '100%', resizeMode: "cover" }} />
+                    :
+                    <>
+                      <Image source={require('../../assets/images/ImageExclamation.svg')} style={{ width: 16, height: 18 }} />
+                      <TextInformation>Nenhuma foto informada</TextInformation>
+                    </>
+
+                  }
+                </ContainerMargin>
+        
+              </ContainerMargin>
+
+              <ContainerMargin $fd="row" $justContent="space-between" $mt={10}>
+        
+                <ButtonGreen onPress={() => { setShowModalCamera(true) }} />
+        
+                <TextCancelAppointment style={{ width: '25%', paddingTop: 10, paddingBottom: 10 }}>Cancelar</TextCancelAppointment>
+        
+              </ContainerMargin>
+            </>
+          ) :
+            <></>
+        }
+
+
 
         <ContainerMargin $mt={30} $gap={30} $mb={30}>
+       
           <ButtonDefault textButton="Salvar" />
 
           <ButtonDefault textButton="Editar" />
 
           <LinkUnderlineDefault onPress={() => { navigation.replace('Home') }}>Cancelar</LinkUnderlineDefault>
+       
         </ContainerMargin>
+      
       </ContainerScrollView>
 
       <ModalCamera showModalCamera={showModalCamera} setShowModalCamera={setShowModalCamera} navigation={navigation} />
+   
     </Container>
   )
 }
