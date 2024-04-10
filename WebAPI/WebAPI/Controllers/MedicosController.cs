713cb9ac7f6508ef4bc4c3bdbd123a9f2d4d2ff7
Allan Rodriues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-10 10:49:12 -0300

    iniciado implementaçao de email na api

--- WebAPI/WebAPI/Controllers/MedicosController.cs
+++ WebAPI/WebAPI/Controllers/MedicosController.cs
@@ -22,23 +22,27 @@ namespace WebAPI.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_medicoRepository.ListarTodos());
+            try
+            {
+                return Ok(_medicoRepository.ListarTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("BuscarPorId")]
         public IActionResult GetById(Guid id)
         {
-           
-            return Ok(_medicoRepository.BuscarPorId(id)); ;
-        }
-
-        [Authorize]
-        [HttpPut]
-        public IActionResult AtualizarPerfil(MedicoViewModel medico)
-        {
-            Guid idUsuario = Guid.Parse(HttpContext.User.Claims.First(c => c.Type == JwtRegisteredClaimNames.Jti).Value);
-
-            return Ok(_medicoRepository.AtualizarPerfil(idUsuario, medico));
+            try
+            {
+                return Ok(_medicoRepository.BuscarPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
@@ -69,14 +73,45 @@ namespace WebAPI.Controllers
         [HttpGet("BuscarPorIdClinica")]
         public IActionResult GetByIdClinica(Guid id)
         {
+            try
+            {
+                return Ok(_medicoRepository.ListarPorClinica(id)); ;
 
-            return Ok(_medicoRepository.ListarPorClinica(id)); ;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("BuscarPorData")]
-        public IActionResult BuscarPorData(DateTime data, Guid id)
+        public IActionResult GetByDate(DateTime data, Guid id)
         {
-            return Ok(_medicoRepository.BuscarPorData(data, id));
+            try
+            {
+                return Ok(_medicoRepository.BuscarPorData(data, id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPut("UpdateProfile")]
+        public IActionResult UpdateProfile(MedicoViewModel medico)
+        {
+            try
+            {
+                Guid idUsuario = Guid.Parse(HttpContext.User.Claims.First(c => c.Type == JwtRegisteredClaimNames.Jti).Value);
+
+                return Ok(_medicoRepository.AtualizarPerfil(idUsuario, medico));
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ WebAPI/WebAPI/Utils/Mail/EmailService.cs
@@ -0,0 +1,56 @@
+﻿
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+
+namespace WebAPI.Utils.Mail
+{
+    public class EmailService : IEmailService
+    {
+        //variavel privada com as configs do email 
+        private readonly EmailSettings emailSettings;
+        public EmailService(IOptions<EmailSettings> options) 
+        { 
+            //obtem as configuraçoes de email e armazena na variavel privada
+            emailSettings = options.Value;
+        }
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            try
+            {
+                //objeto que representa o email
+                var email = new MimeMessage();
+                //Define o remetente do email
+                email.Sender = MailboxAddress.Parse(emailSettings.Email);
+                //Adiciona o destinatario do email
+                email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+                //Define o asso do email
+                email.Subject = mailRequest.Subject;
+
+                //Cria o corpo do email
+                var builder = new BodyBuilder();
+                //Define o corpo do email como html
+                builder.HtmlBody = mailRequest.Body;
+                //define o corpo do email no obj mimeMessage
+                email.Body = builder.ToMessageBody();
+
+                //Cria um client SMTP para envio de email
+                using (var smtp = new SmtpClient())
+                {
+                    //Conecta-se ao servidor SMTP usando os dados do emailSettings
+                    smtp.Connect(emailSettings.Host, emailSettings.Port, SecureSocketOptions.StartTls);
+                    //autentica-se no servidor SMTP usando dados do emailSettings
+                    smtp.Authenticate(emailSettings.Email, emailSettings.Password);
+                    //envia o email assincrono
+                    await smtp.SendAsync(email);
+                }
+            } catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
+}
