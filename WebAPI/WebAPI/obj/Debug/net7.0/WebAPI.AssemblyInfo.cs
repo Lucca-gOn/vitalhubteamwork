df0b141edaa919f47ecef4ea8de86906e09e5fff
Allan Rodriues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 10:10:47 -0300

    Realizado OCR

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042f2c9c65bc069bef17e8c1f62c2c529216eaef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f42e960bb7a6cc9afa8112b39e7cdb4e31a585c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
